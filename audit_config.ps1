7fe650df684bd4036e5e32c1c91265816220ea45
MSKWKR <andrewwang@gmail.com>
2025-07-08 14:30:56 +0800

    fixed syntax error in audit_config

--- audit_config.ps1
+++ audit_config.ps1
@@ -5,12 +5,12 @@ Write-Host "Audit policy config before change:"
 Get-MDIConfiguration -Mode Domain -Configuration All
 
 # List of policies that needs to be set
-$auditList = @[
+$auditList = @(
     "AdvancedAuditPolicyDCs",
     "ConfigurationContainerAuditing",
     "DomainObjectAuditing",
     "NTLMAuditing"
-]
+)
 # Configuring Advanced Audit Policy settings using PowerShell method
 foreach($auditItem in $auditList){
     Set-MDIConfiguration -Mode Domain -Configuration $auditItem -Verbose
