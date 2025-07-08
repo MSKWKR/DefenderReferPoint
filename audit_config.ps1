4b09444ba2496c7eae842c0016ff4c45dd46c1a9
MSKWKR <andrewwang@gmail.com>
2025-07-08 15:56:45 +0800

    Adjusted output

--- audit_config.ps1
+++ audit_config.ps1
@@ -1,8 +1,14 @@
 # This file configures Advanced Audit Policies needed for Defender Identity Sensor
 
-# Display the audit configuration before change
-Write-Host "Audit policy config before change:"
-Get-MDIConfiguration -Mode Domain -Configuration All
+param(
+    [switch]$Revert
+)
+
+# Backup current config into JSON
+$fullConfig = Get-MDIConfiguration -Mode Domain -Configuration All -Identity $mdiIdentity -WarningAction SilentlyContinue
+$backupPath = "$PSScriptRoot\AuditConfigBackup.json"
+$fullConfig | Select-Object Configuration, Status | ConvertTo-Json -Depth 2 | Set-Content -Path $backupPath -Encoding UTF8
+Write-Host "Backup complete. Saved to: $backupPath" -ForegroundColor Green
 
 # List of policies that needs to be set
 $auditList = @(
@@ -11,11 +17,35 @@ $auditList = @(
     "DomainObjectAuditing",
     "NTLMAuditing"
 )
-# Configuring Advanced Audit Policy settings using PowerShell method
-foreach($auditItem in $auditList){
-    Set-MDIConfiguration -Mode Domain -Configuration $auditItem -Verbose
+
+# Applying Advanced Audit Policy settings using PowerShell method
+function Apply-Audit{
+    foreach($auditItem in $auditList){
+        Set-MDIConfiguration -Mode Domain -Configuration $auditItem -Verbose
+    }
+}
+
+# Reverting Advanced Audit Policy settings
+function Revert-Audit{
+    $backupConfig = Get-Content $backupPath | ConvertFrom-Json
+    foreach($entry in $backupConfig){
+        $config = $entry.Configuration
+        $status = $entry.Status
+        # Set-MDIConfiguration -Mode Domain -Configuration $config
+    }
+}
+
+# Display the audit configuration before change
+Write-Host "Audit policy config before change:" -Foreground Cyan
+Get-MDIConfiguration -Mode Domain -Configuration All -Identity FDS-MDI-Autobot -WarningAction SilentlyContinue | Out-Host
+
+if($Revert){
+    Revert-Audit
+}
+else{
+    Apply-Audit
 }
 
 # Display the audit configuration after change
-Write-Host "Audit policy config after change:"
-Get-MDIConfiguration -Mode Domain -Configuration All
+Write-Host "Audit policy config after change:" -Foreground Cyan
+Get-MDIConfiguration -Mode Domain -Configuration All -Identity FDS-MDI-Autobot -WarningAction SilentlyContinue | Out-Host
